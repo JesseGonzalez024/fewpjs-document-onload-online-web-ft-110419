2a38c5bbc9074cc00841992d5d80255bfbc10f29
Jesse Gonzalez <jessegonzalez024@gmail.com>
2020-03-09 15:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Your code goes here
 
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("text").innerHTML = "This is really cool!";
 });
+=======
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+});
+
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+>>>>>>> d55a997ca9f41a3b36ec515cc7247669b8165111
